269ba0d1025d0da7d40e3fcd57015bfaed611a0a
mnguyen175 <maryannguyen2017@gmail.com>
2025-01-16 21:49:12 +0000

    work

--- homework.cpp
+++ homework.cpp
@@ -11,10 +11,31 @@
 #include <string>    // std::string, std::getline
 #include <vector>    // std::vector
 
+// Compare
+void compare(int x, int y)
+{
+    if (x < y)
+    {
+        std::cout << x << " is less than " << y << std::endl;
+    }
+    else if (x > y)
+    {
+        std::cout << x << " is greater than " << y << std::endl;
+    }
+    else 
+    {
+        std::cout << "The inputs are equal" << std::endl;
+    }
+
+// Quadratic Formula
+void quadForm()
+}
 int main()
 {
     // Complete your homework here
-
+    compare(5, 3);
+    compare (2, 6);
+    compare (3, 3);
     
     return 0;
 }
